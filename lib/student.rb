b34bf6f89b685e7d99fac0ab300d52f3ee26516e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:27:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,4 +28,12 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES ( ? , ? )
+    SQL
+
+    DB[:conn].execute(sql, name, grade)
+  end
 end
