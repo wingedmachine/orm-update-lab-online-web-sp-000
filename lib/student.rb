e0034ddbd4e53332d9f6efa7396852004ce06aef
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:18:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,9 @@ require_relative "../config/environment.rb"
 class Student
   attr_reader :id, :name, :grade
 
-  def initialize
+  def initialize(name, grade)
     @id = nil
+    @name = name
+    @grade = grade
   end
 end
