b6dad94b685e78fa1fe86da1a09b9eeba5a47e72
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:39:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,6 +53,16 @@ class Student
     Student.new(row[1], row[2], row[0])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
+
   def update
     sql = <<-SQL
       UPDATE students
